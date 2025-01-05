9fac06268d256ed43449c67a5fbd738834cf2274
kreeteen <krestnikov.dima5@gmail.com>
2025-01-05 19:32:57 +0300

    добавил подсчет нормалей

--- src/main/java/com/cgvsu/math/Vector3f.java
+++ src/main/java/com/cgvsu/math/Vector3f.java
@@ -8,13 +8,11 @@ public class Vector3f {
         this.z = z;
     }
 
-    public boolean equals(Vector3f other) {
-        // todo: желательно, чтобы это была глобальная константа
-        final float eps = 1e-7f;
-        return Math.abs(x - other.x) < eps && Math.abs(y - other.y) < eps && Math.abs(z - other.z) < eps;
-    }
-
     float x, y, z;
+
+    public float getX() {
+        return x;
+    }
 
     public float getY() {
         return y;
@@ -24,7 +22,41 @@ public class Vector3f {
         return z;
     }
 
-    public float getX() {
-        return x;
+    public boolean equals(Vector3f other) {
+        final float eps = 1e-7f;
+        return Math.abs(x - other.x) < eps && Math.abs(y - other.y) < eps && Math.abs(z - other.z) < eps;
+    }
+
+    public Vector3f cross(Vector3f other) {
+        return new Vector3f(
+                y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x
+        );
+    }
+
+    public Vector3f normalize() {
+        float length = (float) Math.sqrt(x * x + y * y + z * z);
+        return new Vector3f(x / length, y / length, z / length);
+    }
+
+    public Vector3f add(Vector3f other) {
+        return new Vector3f(x + other.x, y + other.y, z + other.z);
+    }
+
+    public Vector3f subtract(Vector3f other) {
+        return new Vector3f(x - other.x, y - other.y, z - other.z);
+    }
+
+    public Vector3f multiply(float scalar) {
+        return new Vector3f(x * scalar, y * scalar, z * scalar);
+    }
+
+    public Vector3f divide(float scalar) {
+        return new Vector3f(x / scalar, y / scalar, z / scalar);
+    }
+
+    public Vector3f copy() {
+        return new Vector3f(this.x, this.y, this.z);
     }
 }
--- src/main/java/com/cgvsu/model/Model.java
+++ src/main/java/com/cgvsu/model/Model.java
@@ -10,4 +10,57 @@ public class Model {
     public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
     public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
     public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+
+    public void computeNormals() { //https://github.com/SysoevaSvetlana/CGtask3/blob/main/ObjReaderInitial/src/com/cgvsu/model/Model.java
+
+        Map<Integer, Vector3f> vertexNormals = new HashMap<>();
+        Map<Integer, Integer> vertexNormalsCount = new HashMap<>();
+
+
+        for (Polygon polygon : polygons) {
+            ArrayList<Integer> vertexIndices = polygon.getVertexIndices();
+            if (vertexIndices.size() < 3) {
+                continue;
+            }
+
+            Vector3f v0 = vertices.get(vertexIndices.get(0));
+            Vector3f v1 = vertices.get(vertexIndices.get(1));
+            Vector3f v2 = vertices.get(vertexIndices.get(2));
+
+            Vector3f edge1 = v1.subtract(v0);
+            Vector3f edge2 = v2.subtract(v0);
+            Vector3f faceNormal = edge1.cross(edge2).normalize();
+
+            for (int index : vertexIndices) {
+                vertexNormals.compute(index, (k, v) -> {
+                    if (v == null) {
+                        return faceNormal.copy();
+                    } else {
+                        return v.add(faceNormal);
+                    }
+                });
+            }
+
+            for (int index : vertexIndices) {
+
+                if (vertexNormalsCount.containsKey(index)) {
+                    vertexNormalsCount.put(index, vertexNormalsCount.get(index) + 1);
+                } else {
+                    vertexNormalsCount.put(index,1);
+                }
+
+            }
+        }
+
+
+        for (Integer index : vertexNormals.keySet()) {
+            vertexNormals.put(index, vertexNormals.get(index).divide(vertexNormalsCount.get(index)));
+        }
+
+
+        normals = new ArrayList<>();
+        for (int i = 0; i < vertices.size(); i++) {
+            normals.add(vertexNormals.getOrDefault(i, new Vector3f(0, 0, 0)));
+        }
+    }
 }
